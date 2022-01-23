0586a5e2ddd8ace54b3158508230b651ebfebef7
Adam DePaolo <amdepaolo@gmail.com>
2022-01-23 16:09:29 -0500

    commit tree 8492c0335f1bee7f3e103726d5ff3932725931a2

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "full name",
+    streetAddress: "Address"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee,
+    [key]:value,};
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+};
+function deleteFromEmployeeByKey(employee, key){
+    const clone = {...employee};
+    delete clone[key];
+    return clone;
+};
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
